f126d246fa5fbe41443bed6d3ed20e55e7010cfd
ifury77 <sgp69k@gmail.com>
2026-04-20 00:17:15 +0800

    Fix: remove duplicate authOptions imports

--- app/api/gmail/search/route.js
+++ app/api/gmail/search/route.js
@@ -1,6 +1,5 @@
 ﻿import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
 
 export const maxDuration = 30;
 
